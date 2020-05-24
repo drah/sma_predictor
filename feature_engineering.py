5451f59b0bcb0bcbd0bb553467853cefc0b63071
drah <drah2easy@gmail.com>
2020-05-24 22:56:24 +0800

    simply remove date

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,5 +1,9 @@
 
-
+def handle_features(data):
+  for features in data:
+    features.pop(0)
+    for i in range(len(features)):
+      features[i] = float(features[i])
 
 if __name__ == '__main__':
   pass
