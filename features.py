ef36760bf2a519280f947c5b812d10b1c3a5f85d
drah <drah2easy@gmail.com>
2020-05-29 06:32:15 +0800

    features: handle date

--- features.py
+++ features.py
@@ -1,10 +1,24 @@
 import numpy as np
 
-def handle_features(data):
+def handle_features(data, mean=None, std=None):
+  months = []
+  days = []
   for features in data:
-    features.pop(0)
+    date = features[0].split('-')
+    features[0] = float(date[0])
+    months.append(date[1])
+    days.append(date[2])
+
+  stat = get_stat(data)
+  mean = mean if mean is not None else stat['mean']
+  std = std if std is not None else stat['std']
+  for month, day, features in zip(months, days, data):
     for i in range(len(features)):
       features[i] = float(features[i])
+    _standardize(features, mean, std)
+    features.extend(one_hot(int(month)-1, 12))
+    features.extend(one_hot(int(day)-1, 31))
+  return stat
 
 def get_stat(data):
   data = np.array(data)
@@ -16,8 +30,14 @@ def get_stat(data):
 
 def standardize(data, mean, std):
   for features in data:
-    for i in range(len(features)):
-      features[i] = (features[i] - mean[i]) / std[i]
+    _standardize(features, mean, std)
+
+def _standardize(features, mean, std):
+  for i in range(len(features)):
+    features[i] = (features[i] - mean[i]) / std[i]
+
+def one_hot(data, depth):
+  return [0 if i != data else 1 for i in range(depth)]
 
 if __name__ == '__main__':
   def test():
@@ -28,9 +48,8 @@ if __name__ == '__main__':
     labels = reader.generate_SMA(spy.data, 4, 30)
     train, val, test = data_pipeline.split_interval(
         spy.data, labels, [0.7, 0.15, 0.15])
+    pprint(train.data[:5])
     handle_features(train.data)
-    stat = get_stat(train.data)
-    standardize(train.data, stat['mean'], stat['std'])
     pprint(train.data[:5])
     pprint(get_stat(train.labels))
 
